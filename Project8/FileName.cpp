923b671686f23d1805ed77329a1e668ed704899a
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-23 21:21:27 +0900

    초기 커밋.

---
+++ Project8/FileName.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    string user_id;
+    cout << "ID: ";
+    cin >> user_id;
+
+    vector<string> problem_nums;
+    string problem_num;
+    cout << "Problem Number(Input until 0): ";
+    while (1)
+    {
+        cin >> problem_num;
+        if (problem_num == "0") // 0 입력되면 종료
+            break;
+        problem_nums.push_back(problem_num);
+    }
+
+    string url, cmd;
+    // 각 문제 번호에 대한 url 열기
+    for (auto& num : problem_nums)
+    {
+        url = "https://www.acmicpc.net/status?from_mine=1&problem_id=" + num + "&user_id=" + user_id;
+        cmd = "start chrome \"" + url + "\"";
+        system(cmd.c_str());
+    }
+    return 0;
+}
